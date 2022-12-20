c9031fe19a1e4fca3266d6c8831d46decf827257
SwapnanilGhosh <swapnanil2002@gmail.com>
2022-12-20 11:30:36 +0530

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -303,3 +303,33 @@ function showmenu()
         x.style.display = 'block';
     }
 }
+
+
+$(".modal-transparent").on('show.bs.modal', function () {
+    setTimeout( function() {
+      $(".modal-backdrop").addClass("modal-backdrop-transparent");
+    }, 0);
+  });
+  $(".modal-transparent").on('hidden.bs.modal', function () {
+    $(".modal-backdrop").addClass("modal-backdrop-transparent");
+  });
+  
+  $(".modal-fullscreen").on('show.bs.modal', function () {
+    setTimeout( function() {
+      $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+    }, 0);
+  });
+  $(".modal-fullscreen").on('hidden.bs.modal', function () {
+    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+  });
+  
+   $(window).on('load', function() {
+          $('#modal-fullscreen').modal('show');
+      });
+  
+  
+  
+  
+  setTimeout(function(){
+    $('#modal-fullscreen').modal('hide')
+  }, 11000);
